64df0ffe007119bbb9dac2a637d18f8ee1f93eea
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2025-01-30 18:30:10 +0530

    complet siginUp page funtinality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,41 +1,77 @@
-import React from "react";
+import React, { useState, useTransition } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const handleOnChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  console.log("this is form data", formData);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    startTransition(async () => {
+      try {
+        const data = await fetch("/api/auth/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const res = await data.json();
+        console.log(res)
+        if (!res.success) return setError(true);
+        setError(false);
+      } catch (error) {
+        setError(true);
+      }
+    });
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center my-7 font-semibold">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleOnChange}
         />
         <input
           type="email"
           placeholder="email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleOnChange}
         />
         <input
           type="password"
           placeholder="password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleOnChange}
         />
         <button
-          disabled
+          disabled={isPending}
+          type="submit"
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          Sign Up
+          {isPending ? "Please wait" : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account? </p>
-        <Link to='/sign-in'>
+        <Link to="/sign-in">
           <span className="text-blue-500">Sign In</span>
         </Link>
+      </div>
+      <div>
+        <p className="text-red-700 mt-5">{error && "Something went wrong"}</p>
       </div>
     </div>
   );
