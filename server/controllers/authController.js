7dcf8d82194bb3df23cfee9671540148df0653cc
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2025-01-30 21:49:54 +0530

    complete the signIn api in backend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,8 @@
 import { catchAsyncError } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/error.js";
 import User from "../models/user.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const signUp = catchAsyncError(async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,4 +13,18 @@ export const signUp = catchAsyncError(async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   await newUser.save();
   return res.status(201).json({ message: "User Created Successfully" });
+});
+
+export const signIn = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!(email || password))
+    return next(new ErrorHandler("Email and Password is require", 400));
+  const vaildEmail = await User.findOne({ email });
+  if (!vaildEmail) return next(new ErrorHandler("Email not register", 400));
+  const validPassword = await bcrypt.compare(password, vaildEmail.password);
+  if (!validPassword) return next(new ErrorHandler("Incorrect Password", 400));
+  const payload = { id: vaildEmail._id };
+  const token = jwt.sign(payload, process.env.JWT_TOKEN, { expiresIn: "1hr" });
+  const {password: userPassword, ...user} = vaildEmail._doc;
+  return res.cookie("Token", token, { httpOnly: true, expires: new Date(Date.now() + 3600000) }).status(200).json({Data: {...user} ,message: 'Logged In Successfully'});
 });
--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import dbConnection from "./database/dbConnection.js";
 dotenv.config();
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 import { errorMiddleware } from "./middlewares/error.js";
 import authRoute from "./routes/authRoute.js";
