96d66306c769d8df9b1d57646f7465faca1e136f
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2025-02-17 17:23:44 +0530

    profile page ui completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../features/auth/authSlice';
 
 const Profile = () => {
+
+  const user = useSelector(selectCurrentUser);
   return (
-    <div>
-      
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'> Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img src={user.profileImg} alt='Profile Picture' className='h-24 w-24 self-center cursor-pointer
+        rounded-full object-cover mt-2'></img>
+        <input defaultValue={user.username} type='text' id='username' placeholder='Username'
+        className='bg-slate-100 rounded-tr-lg rounded-bl-lg p-2'/>
+        <input defaultValue={user.email} type='email' id='email' placeholder='Email'
+        className='bg-slate-100 rounded-tr-lg rounded-bl-lg p-2'/>
+        <input type='password' id='password' placeholder='Password'
+        className='bg-slate-100 rounded-tr-lg rounded-bl-lg p-2'/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+      </form>
+        <div className='flex justify-between mt-5'>
+          <span className='text-red-700 cursor-pointer'>Delete Account</span>
+          <span className='text-red-700 cursor-pointer'>Sign Out</span>
+        </div>
     </div>
   )
 }
