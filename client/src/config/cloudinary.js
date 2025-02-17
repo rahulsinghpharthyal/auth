02603ed13beee9f8260146238324258ef8fab47a
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2025-02-17 22:34:50 +0530

    complete upload image to cloudinary from only frontend

---
+++ client/src/config/cloudinary.js
@@ -0,0 +1,4 @@
+export const cloudinaryConfig = {
+    cloud_name: 'dg56sdt6k',
+    upload_preset: 'image_upload' //Create an unsigned upload preset and update this
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,30 +1,107 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { selectCurrentUser } from '../features/auth/authSlice';
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../features/auth/authSlice";
+import { useEffect, useRef, useState } from "react";
+import { cloudinaryConfig } from "../config/cloudinary";
+import axios from "axios";
 
 const Profile = () => {
+  console.log(cloudinaryConfig);
+  const fileRef = useRef(null);
+  const [image, setImage] = useState();
+  const [imageUrl, setImageUrl] = useState("");
+  const [message, setMessage] = useState("");
 
+  const [formData, setFormData] = useState({});
+  console.log("this is image", image);
   const user = useSelector(selectCurrentUser);
+
+  // uplaod to cloudinary:-
+  const handleUploadImageToCloudinary = async (image) => {
+    try {
+      if (!image) {
+        return;
+      }
+      const formData = new FormData();
+      formData.append("file", image);
+      formData.append("upload_preset", cloudinaryConfig.upload_preset);
+      const response = await axios.post(
+        `https://api.cloudinary.com/v1_1/${cloudinaryConfig.cloud_name}/image/upload`,
+        formData
+      );
+      setFormData({...formData, profileImg: response?.data?.secure_url})
+      setImageUrl(response?.data?.secure_url);
+      console.log("this is response", response);
+      setMessage("Image Uploaded Successfully!");
+    } catch (error) {
+      console.log('this is error', error)
+      setMessage(error?.response?.data?.error?.message || 'Error uploading image')
+    }
+  };
+
+  console.log('this is fomrm Data', formData)
+
+  if (message) {
+    setTimeout(() => {
+      setMessage("");
+    }, 6000);
+  }
+  useEffect(() => {
+    if (image) {
+      handleUploadImageToCloudinary(image);
+    }
+  }, [image]);
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'> Profile</h1>
-      <form className='flex flex-col gap-4'>
-        <img src={user.profileImg} alt='Profile Picture' className='h-24 w-24 self-center cursor-pointer
-        rounded-full object-cover mt-2'></img>
-        <input defaultValue={user.username} type='text' id='username' placeholder='Username'
-        className='bg-slate-100 rounded-tr-lg rounded-bl-lg p-2'/>
-        <input defaultValue={user.email} type='email' id='email' placeholder='Email'
-        className='bg-slate-100 rounded-tr-lg rounded-bl-lg p-2'/>
-        <input type='password' id='password' placeholder='Password'
-        className='bg-slate-100 rounded-tr-lg rounded-bl-lg p-2'/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7"> Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+        <img
+          src={imageUrl ? imageUrl : user.profileImg}
+          alt="Profile Picture"
+          className="h-24 w-24 self-center cursor-pointer
+        rounded-full object-cover mt-2"
+          onClick={() => fileRef.current.click()}
+        ></img>
+        {message && <p className="text-center text-green-400">{message}</p>}
+        <input
+          defaultValue={user.username}
+          type="text"
+          id="username"
+          placeholder="Username"
+          onChange={(e)=>setFormData({...formData, username: e.target.value})}
+          className="bg-slate-100 rounded-tr-lg rounded-bl-lg p-2"
+        />
+        <input
+          defaultValue={user.email}
+          type="email"
+          id="email"
+          placeholder="Email"
+          onChange={(e)=>setFormData({...formData, email: e.target.value})}
+          className="bg-slate-100 rounded-tr-lg rounded-bl-lg p-2"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          onChange={(e)=>setFormData({...formData, password: e.target.value})}
+          className="bg-slate-100 rounded-tr-lg rounded-bl-lg p-2"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
       </form>
-        <div className='flex justify-between mt-5'>
-          <span className='text-red-700 cursor-pointer'>Delete Account</span>
-          <span className='text-red-700 cursor-pointer'>Sign Out</span>
-        </div>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Profile;
