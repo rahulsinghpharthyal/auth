e6e5d8f81312768358a7492f8f3560f7bd07c5bb
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2025-02-19 22:42:17 +0530

    add in local the serviec account file

--- server/utils/firebaseConfigration.js
+++ server/utils/firebaseConfigration.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from './serviseAccountKey.json' with {type:"json"};
+import serviceAccount from './serviceAccountKey.json' with {type:"json"};
 
 const fireBase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
